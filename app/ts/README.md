6bebe3254df7c3f8c1dc032450b019ab02030c26
emmeiwhite <emmeiwhite@gmail.com>
2025-07-01 19:39:34 +0530

    Generic Function

---
+++ app/ts/README.md
@@ -0,0 +1,7 @@
+## GENERICS: 💡 Definition:
+
+`Generics allow you to write reusable, type-safe code where the type is not known until usage.`
+
+### 🔍 Analogy:
+
+`Think of generics like function parameters, but for types.`
--- app/ts/page.tsx
+++ app/ts/page.tsx
@@ -16,6 +16,54 @@ const roufi: Employee = {
   email: 'roufi@gmail.com',
   employeeId: 11
 }
+
+// Generics: Generics allows you to write resuable, type-safe code, where the type is not known until usage.
+
+function logString(arg: string) {
+  console.log(arg)
+  return arg
+}
+
+logString('Pray, Move & Act. Motivation will find you on the road')
+
+// Now a new requirement came to log number
+
+function logNumber(arg: number) {
+  console.log(arg)
+  return arg
+}
+
+logNumber(23)
+
+// Assume yet a new requirement arises to log Boolean
+
+function logBoolean(arg: boolen) {
+  console.log(arg)
+  return arg
+}
+
+logBoolean(true)
+
+// BUT WE WILL CREATE A GENERIC FUNCTION WHICH WILL AUTOMATICALLY INFER THE TYPE AT RUN TIME
+
+function logAnything<T>(arg: T): T {
+  console.log(arg)
+  return arg
+}
+
+logAnything([2, 4, 6])
+logAnything('45')
+logAnything<number>(345)
+
+// New requirement logArr
+
+function logArray(arg: any[]) {
+  console.log(arg)
+  return arg
+}
+
+logArray([4, 6, 7])
+
 export default function TypeScript() {
   const { username, age, email, employeeId } = roufi
   return (
@@ -29,6 +77,8 @@ export default function TypeScript() {
         <p>{email}</p>
         <p>{employeeId}</p>
       </div>
+
+      <h2>{logNumber(45)}</h2>
     </div>
   )
 }
