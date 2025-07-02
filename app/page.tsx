2ee64806a83b57add9363662ff522d19a43596f4
emmeiwhite <emmeiwhite@gmail.com>
2025-07-02 11:53:53 +0530

    setup ready with data.ts

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import Home from '@/components/Home/Home'
+
 export default async function HomePage() {
-  return <div className="text-5xl">HomePage</div>
+  return (
+    <div className="text-5xl">
+      <Home />
+    </div>
+  )
 }
