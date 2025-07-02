91a7d576c70180b3790ee377e080c299832cbc2f
emmeiwhite <emmeiwhite@gmail.com>
2025-07-02 12:38:14 +0530

    Icon with text in the nav

--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -1,3 +1,21 @@
+import { TbAirBalloon } from 'react-icons/tb'
+
 export default function Nav() {
-  return <div>Nav</div>
+  return (
+    <div className="bg-blue-950 transition-all duration-200 h-[12vh]  ">
+      {/* Layout Container */}
+
+      <div className="max-w-5xl mx-auto px-6 flex items-center h-full">
+        {/* LOGO */}
+        <div className="flex items-center gap-2">
+          {/* Icon */}
+          <div className="h-10 w-10 bg-rose-500 rounded-full  flex items-center justify-center">
+            <TbAirBalloon className="h-6 w-6 text-white" />
+          </div>
+          {/* Icon Text */}
+          <p className="text-xl md:text-2xl font-bold text-white font-bold">Tripi</p>
+        </div>
+      </div>
+    </div>
+  )
 }
